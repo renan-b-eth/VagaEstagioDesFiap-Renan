065108565c4492dfa305f05cc33bd2a0243c8adb
renan-b-eth <renanacademic21@gmail.com>
2024-06-21 20:20:29 -0300

    base feita

---
+++ fiap-prova-renan/src/app/page.tsx
@@ -0,0 +1,32 @@
+
+
+export default function Home() {
+  return (
+    <>
+      <html>
+        <head>
+         <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
+          <title>FIAP</title>
+        </head>
+
+        <body>
+    <header>
+        <nav id="navbar">
+        </nav>
+    </header>
+
+    <main>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sed lectus nec augue semper varius. Donec ut quam nec sem tincidunt ullamcorper. Sed euismod leo eget ligula efficitur, at congue metus ultrices. Nullam ac leo vitae dolor ullamcorper hendrerit. Maecenas nec enim eget mi tempor ullamcorper at quis lectus. Sed at est eu quam congue mollis eget ac libero. Nullam sit amet lectus eu elit tincidunt tincidunt.</p>
+        <p>Praesent vitae ligula ac enim semper rutrum. Maecenas non enim ac ligula tincidunt ultrices quis ut velit. Donec ullamcorper augue et dui porta, sit amet congue velit hendrerit. Morbi sit amet augue ut quam pretium aliquam eget quis dui. Sed ac dui ac libero sodales feugiat. Cras a neque ac lectus consectetur hendrerit.</p>
+        <p>Nulla facilisi. Nam ac diam eget odio convallis semper. Sed vitae enim vitae purus tempor semper. Duis sed quam et ligula semper feugiat. Sed ultrices dui non leo semper, eget interdum tellus ultrices. Maecenas sit amet orci eu ipsum tincidunt ullamcorper.</p>
+        <p>Morbi eget ligula eget ipsum ultrices aliquam. In hac habitasse platea dictumst. Vivamus et risus velit. Maecenas nec enim eget mi tempor ullamcorper at quis lectus. Sed at est eu quam congue mollis eget ac libero. Nullam sit amet lectus eu elit tincidunt tincidunt.</p>
+        <p>Praesent vitae ligula ac enim semper rutrum. Maecenas non enim ac ligula tincidunt ultrices quis ut velit. Donec ullamcorper augue et dui porta, sit amet congue velit hendrerit. Morbi sit amet augue ut quam pretium aliquam eget quis dui. Sed ac dui ac libero sodales feugiat. Cras a neque ac lectus consectetur hendrerit.</p>
+        <p>Nulla facilisi. Nam ac diam eget odio convallis semper. Sed vitae enim vitae purus tempor semper. Duis sed quam et ligula semper feugiat. Sed ultrices dui non leo semper, eget interdum tellus ultrices. Maecenas sit amet orci eu ipsum tincidunt ullamcorper.</p>
+    </main>
+
+    </body>
+
+      </html>
+    </>
+  );
+}
